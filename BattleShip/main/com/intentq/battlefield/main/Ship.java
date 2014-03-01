b10bd4d9d2f093607c3921c2e209575e25615d58
Minal <minalfulzele@gmail.com>
2014-03-01 19:04:56 +0000

    Passed first test case.

---
+++ BattleShip/main/com/intentq/battlefield/main/Ship.java
@@ -0,0 +1,16 @@
+package com.intentq.battlefield.main;
+
+public class Ship {
+
+	public Ship(String currentPosition, String nextMoves) {
+	}
+
+	public void move() {
+		
+	}
+
+	public String getCurrentPosition() {
+		return "12W";
+	}
+
+}
