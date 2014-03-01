15876aab0badc995956f323c3de0f5bba8aee7a7
Minal <minalfulzele@gmail.com>
2014-03-01 20:54:00 +0000

    Added test case {programming by intent} GIVEN_TwoShips_WithMoveSequenceAndShots_The2ndShipShotHits1st_THEN_FirstShipSank : RED

---
+++ BattleShip/test/com/intentq/battlefield/test/TestBattleShipShots.java
@@ -0,0 +1,35 @@
+package com.intentq.battlefield.test;
+
+import static org.junit.Assert.*;
+import static  com.intentq.battlefield.main.Move.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.intentq.battlefield.main.Move;
+import com.intentq.battlefield.main.Orientation;
+import com.intentq.battlefield.main.Position;
+import com.intentq.battlefield.main.Ship;
+
+public class TestBattleShipShots {
+
+	private List<Move> nextMoveSequence(Move...actions) {
+		return Arrays.asList(actions);
+	}
+	
+	@Test
+	public void GIVEN_TwoShips_WithMoveSequenceAndShots_The2ndShipShotHits1st_THEN_FirstShipSank() {
+		
+		List<Ship> ships = new ArrayList<Ship>();
+		ships.add(new Ship(new Position(1,2,Orientation.N),nextMoveSequence(L,M,L,M,L,M,L,M,M)));
+		ships.add(new Ship(new Position(3,3,Orientation.E),nextMoveSequence(M,M,R,M,M,R,M,R,R,M)));
+		PlayGame game = new PlayGame(new Grid(5,5),ships);
+		assertTrue(LifeStatus.SUNK,ships.get(0).getStatus());
+		assertTrue(LifeStatus.ALIVE,ships.get(1).getStatus());
+		
+	}
+
+}
