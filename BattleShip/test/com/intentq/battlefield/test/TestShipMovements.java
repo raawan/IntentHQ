e9325828192f23bd2fd721d3a4d3c14fdf50159b
Minal <minalfulzele@gmail.com>
2014-03-01 19:55:30 +0000

    Added remaining set of test cases for ship movements in different orientation

--- BattleShip/test/com/intentq/battlefield/test/TestShipMovements.java
+++ BattleShip/test/com/intentq/battlefield/test/TestShipMovements.java
@@ -13,7 +13,7 @@ public class TestShipMovements {
 
 
 	@Test
-	public void GIVEN_validShipCoordinates_and_leftMove_THEN_shipRotatesLeft() {
+	public void GIVEN_validShipCoordinates_and_LeftMove_THEN_shipRotatesLeft() {
 		Ship ship = new Ship(new Position(1,2,Orientation.N),Action.L);
 		ship.move();
 		assertEquals(Orientation.W,ship.getCurrentPositionObject().getOrientation());
@@ -35,10 +35,38 @@ public class TestShipMovements {
 	}
 	
 	@Test
-	public void GIVEN_ShipOrientationSouth_and_LEftMove_THEN_shipOrientationEast() {
+	public void GIVEN_ShipOrientationSouth_and_LeftMove_THEN_shipOrientationEast() {
 		Ship ship = new Ship(new Position(1,2,Orientation.S),Action.L);
 		ship.move();
 		assertEquals(Orientation.E,ship.getCurrentPositionObject().getOrientation());
 	}
 	
+	@Test
+	public void GIVEN_ShipOrientationWest_and_LeftMove_THEN_shipOrientationSouth() {
+		Ship ship = new Ship(new Position(1,2,Orientation.W),Action.L);
+		ship.move();
+		assertEquals(Orientation.S,ship.getCurrentPositionObject().getOrientation());
+	}
+	
+	@Test
+	public void GIVEN_ShipOrientationWest_and_RightMove_THEN_shipOrientationNorth() {
+		Ship ship = new Ship(new Position(1,2,Orientation.W),Action.R);
+		ship.move();
+		assertEquals(Orientation.N,ship.getCurrentPositionObject().getOrientation());
+	}
+	
+
+	@Test
+	public void GIVEN_ShipOrientationEast_and_LeftMove_THEN_shipOrientationNorth() {
+		Ship ship = new Ship(new Position(1,2,Orientation.W),Action.L);
+		ship.move();
+		assertEquals(Orientation.S,ship.getCurrentPositionObject().getOrientation());
+	}
+	
+	@Test
+	public void GIVEN_ShipOrientationEast_and_RightMove_THEN_shipOrientationSouth() {
+		Ship ship = new Ship(new Position(1,2,Orientation.W),Action.R);
+		ship.move();
+		assertEquals(Orientation.N,ship.getCurrentPositionObject().getOrientation());
+	}
 }
