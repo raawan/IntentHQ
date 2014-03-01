3f70de04c79ed14f914e5655a5658335ebf65938
Minal <minalfulzele@gmail.com>
2014-03-01 23:52:12 +0000

    Added a test suite encompassing all the tests

---
+++ BattleShip/test/com/intentq/battlefield/test/AllTests.java
@@ -0,0 +1,11 @@
+package com.intentq.battlefield.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestBattleShipShots.class, TestShipMovements.class })
+public class AllTests {
+
+}
