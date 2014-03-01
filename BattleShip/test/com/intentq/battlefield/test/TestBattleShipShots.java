3e46026196c19e1c72405060e8e2d43d181cb69b
Minal <minalfulzele@gmail.com>
2014-03-01 23:44:13 +0000

    Additional test case

--- BattleShip/test/com/intentq/battlefield/test/TestBattleShipShots.java
+++ BattleShip/test/com/intentq/battlefield/test/TestBattleShipShots.java
@@ -40,5 +40,37 @@ public class TestBattleShipShots {
 		assertEquals(LifeStatus.ALIVE,ship1.getLifeStatus());
 		
 	}
-
+	
+	@Test
+	public void GIVEN_TwoShips_WithMoveSequenceAndShots_The2ndShipSHotButDidntHitTheFirstOne_THEN_BothShipsAreAlive() {
+		
+		List<Ship> ships = new ArrayList<Ship>();
+		
+		ships.add(new Ship(new Position(1,2,Orientation.N),nextMoveSequence(L,M,L,M,L,M,L,M,M),new ShotCoordinate(2,3),1));
+		ships.add(new Ship(new Position(3,3,Orientation.E),nextMoveSequence(M,M,R,M,M,R,M,R,R,M),new ShotCoordinate(2,3),2));
+		PlayGame game = new PlayGame(new Grid(5,5),ships);
+		game.play();
+		assertEquals(LifeStatus.ALIVE,ships.get(0).getLifeStatus());
+		assertEquals(LifeStatus.ALIVE,ships.get(1).getLifeStatus());
+	}
+	
+	@Test
+	public void GIVEN_TwoShips_WithMoveSequenceAndShots_AndAnotherSequenceAndSHots_THEN_GameCanBePlayed() {
+		
+		List<Ship> ships = new ArrayList<Ship>();
+		
+		ships.add(new Ship(new Position(1,2,Orientation.N),nextMoveSequence(L,M,L,M,L,M,L,M,M),new ShotCoordinate(2,3),1));
+		ships.add(new Ship(new Position(3,3,Orientation.E),nextMoveSequence(M,M,R,M,M,R,M,R,R,M),new ShotCoordinate(2,3),2));
+		PlayGame game = new PlayGame(new Grid(5,5),ships);
+		game.play();
+		assertEquals(LifeStatus.ALIVE,ships.get(0).getLifeStatus());
+		assertEquals(LifeStatus.ALIVE,ships.get(1).getLifeStatus());
+		
+		ships.get(0).setNextMoves(null);
+		ships.get(1).setShot(new ShotCoordinate(1,3));
+		game.play();
+		assertEquals(LifeStatus.SUNK,ships.get(0).getLifeStatus());
+		assertEquals(LifeStatus.ALIVE,ships.get(1).getLifeStatus());
+		
+	}
 }
