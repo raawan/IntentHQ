a46c83876d2a0b111966352e4ec0d45609a670b6
Minal <minalfulzele@gmail.com>
2014-03-02 23:39:04 +0000

    GIVEN_GridInputInSTringFormat_THEN_convertIntoGridObject_3 : Green

--- BattleShip/main/com/intentq/battlefield/main/Converter.java
+++ BattleShip/main/com/intentq/battlefield/main/Converter.java
@@ -5,10 +5,12 @@ import com.intentq.battlefield.dto.Grid;
 public class Converter {
 
 	public Grid convertGridStringInputToGridObject(String gridCoordinateInString) {
-		StringBuilder strBuilder = new StringBuilder(gridCoordinateInString);
-		int x = Integer.parseInt(strBuilder.charAt(1)+"");
-		int y = Integer.parseInt(strBuilder.charAt(3)+"");
 		
+		StringBuilder strBuilder = new StringBuilder(gridCoordinateInString.trim());
+		int indexOfComma = strBuilder.indexOf(",");
+		int length = strBuilder.length();
+		int x= Integer.parseInt(strBuilder.substring(1, indexOfComma));
+		int y= Integer.parseInt(strBuilder.substring(indexOfComma+1,length-1));
 		return new Grid(x,y);
 	}
 
