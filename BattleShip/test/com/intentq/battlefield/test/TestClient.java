56aabd4b54518e2e7df9e60e7ebcce6009209752
Minal <minalfulzele@gmail.com>
2014-03-02 23:37:53 +0000

    GIVEN_GridInputInSTringFormat_THEN_convertIntoGridObject_3 : RED

--- BattleShip/test/com/intentq/battlefield/test/TestClient.java
+++ BattleShip/test/com/intentq/battlefield/test/TestClient.java
@@ -11,7 +11,7 @@ public class TestClient {
 
 	@Test
 	public void GIVEN_GridCoordinatesAsStringInput_THEN_convertToGridObject() {
-		String gridCoordinateInString = "(5x7)";
+		String gridCoordinateInString = "(5,7)";
 		Grid grid = new Converter().convertGridStringInputToGridObject(gridCoordinateInString);
 		assertEquals(5, grid.getMaxCoOrdinates().getX());
 		assertEquals(7, grid.getMaxCoOrdinates().getY()); 
@@ -19,10 +19,18 @@ public class TestClient {
 	
 	@Test
 	public void GIVEN_GridCoordinatesAsStringInput_THEN_convertToGridObject_2() {
-		String gridCoordinateInString = "(6x8)";
+		String gridCoordinateInString = "(6,8)";
 		Grid grid = new Converter().convertGridStringInputToGridObject(gridCoordinateInString);
 		assertEquals(6, grid.getMaxCoOrdinates().getX());
 		assertEquals(8, grid.getMaxCoOrdinates().getY()); 
 	}
 	
+	@Test
+	public void GIVEN_GridInputInSTringFormat_THEN_convertIntoGridObject_3() {
+		
+		String gridCoordinateInString = "(51,789)";
+		Grid grid = new Converter().convertGridStringInputToGridObject(gridCoordinateInString);
+		assertEquals(51, grid.getMaxCoOrdinates().getX());
+		assertEquals(789, grid.getMaxCoOrdinates().getY()); 
+	}
 }
