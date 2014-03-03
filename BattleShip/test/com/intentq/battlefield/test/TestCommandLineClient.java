4b62686655b39b4582931a5a28f9fade9e6ece8e
Minal <minalfulzele@gmail.com>
2014-03-03 16:20:02 +0000

    GIVEN_GridInputFromCommandLine_THEN_GetValidatedGridObjectFromConverter : red

---
+++ BattleShip/test/com/intentq/battlefield/test/TestCommandLineClient.java
@@ -0,0 +1,22 @@
+package com.intentq.battlefield.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.intentq.battlefield.dto.Grid;
+import com.intentq.battlefield.main.Converter;
+import com.intentq.battlefield.main.IConverter;
+import com.intentq.battlefield.util.Validator;
+
+public class TestCommandLineClient {
+	
+	@Test
+	public void GIVEN_GridInputFromCommandLine_THEN_GetValidatedGridObjectFromConverter() {
+		CommandLineClient cmd = new CommandLineCLient();
+		String gridFrmCommandLine = cmd.getGridCoordinateFromCmdLine();
+		IConverter converter = new Converter(new Validator());
+		Grid grid = converter.convertGridStringInputToGridObject(gridFrmCommandLine);
+		assertNotNull(grid);
+	}
+}
