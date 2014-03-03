b2941c7dd21f62662bbe0bcae127bf259f893321
Minal <minalfulzele@gmail.com>
2014-03-03 18:58:15 +0000

    Removed the integration test from the AllTests.java as it is no longer required

--- BattleShip/test/com/intentq/battlefield/test/AllTests.java
+++ BattleShip/test/com/intentq/battlefield/test/AllTests.java
@@ -6,7 +6,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ TestPlayGame.class, TestShipMovements.class,
-	TestInputConversionAndValidation.class, TestCommandLineClientIntegrationTest.class})
+	TestInputConversionAndValidation.class})
 public class AllTests {
 
 }
