79c31a5df2f758c9ce7bc90c6229136aece04ce3
Minal <minalfulzele@gmail.com>
2014-03-03 07:50:19 +0000

    TestClient refactored

--- BattleShip/test/com/intentq/battlefield/test/TestClient.java
+++ BattleShip/test/com/intentq/battlefield/test/TestClient.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.intentq.battlefield.constants.Move;
@@ -15,11 +16,18 @@ import com.intentq.battlefield.exception.InvalidInputException;
 import com.intentq.battlefield.main.Converter;
 
 public class TestClient {
-
+	
+	private Converter converter;
+	
+	@Before
+	public void setUp() {
+		converter = new Converter();
+	}
+	
 	@Test
 	public void GIVEN_GridCoordinatesAsStringInput_THEN_convertToGridObject() {
 		String gridCoordinateInString = "(5,7)";
-		Grid grid = new Converter().convertGridStringInputToGridObject(gridCoordinateInString);
+		Grid grid = converter.convertGridStringInputToGridObject(gridCoordinateInString);
 		assertEquals(5, grid.getMaxCoOrdinates().getX());
 		assertEquals(7, grid.getMaxCoOrdinates().getY()); 
 	}
@@ -27,7 +35,7 @@ public class TestClient {
 	@Test
 	public void GIVEN_GridCoordinatesAsStringInput_THEN_convertToGridObject_2() {
 		String gridCoordinateInString = "(6,8)";
-		Grid grid = new Converter().convertGridStringInputToGridObject(gridCoordinateInString);
+		Grid grid = converter.convertGridStringInputToGridObject(gridCoordinateInString);
 		assertEquals(6, grid.getMaxCoOrdinates().getX());
 		assertEquals(8, grid.getMaxCoOrdinates().getY()); 
 	}
@@ -36,7 +44,7 @@ public class TestClient {
 	public void GIVEN_GridInputInSTringFormat_THEN_convertIntoGridObject_3() {
 		
 		String gridCoordinateInString = "(51,789)";
-		Grid grid = new Converter().convertGridStringInputToGridObject(gridCoordinateInString);
+		Grid grid = converter.convertGridStringInputToGridObject(gridCoordinateInString);
 		assertEquals(51, grid.getMaxCoOrdinates().getX());
 		assertEquals(789, grid.getMaxCoOrdinates().getY()); 
 	}
@@ -45,7 +53,7 @@ public class TestClient {
 	public void GIVEN_GridInputInSTringFormat_THEN_convertIntoGridObject_4() {
 		
 		String gridCoordinateInString = "(531,7289)";
-		Grid grid = new Converter().convertGridStringInputToGridObject(gridCoordinateInString);
+		Grid grid = converter.convertGridStringInputToGridObject(gridCoordinateInString);
 		assertEquals(531, grid.getMaxCoOrdinates().getX());
 		assertEquals(7289, grid.getMaxCoOrdinates().getY()); 
 	}
@@ -53,7 +61,7 @@ public class TestClient {
 	@Test
 	public void GIVEN_ShipStartingPositionInStringFormat_THEN_convertIntoPositionObject() {
 		String shipCoordinatesAndOrientation = "(1,2,N)";
-		List<Position> pos = new Converter().convertShipPositionStringInputToPositionObject(shipCoordinatesAndOrientation);
+		List<Position> pos = converter.convertShipPositionStringInputToPositionObject(shipCoordinatesAndOrientation);
 		assertEquals(1, pos.get(0).getCurrentCoordinates().getX());
 		assertEquals(2, pos.get(0).getCurrentCoordinates().getY());
 		assertEquals(Orientation.N,pos.get(0).getOrientation());
@@ -62,7 +70,7 @@ public class TestClient {
 	@Test
 	public void GIVEN_ShipStartingPositionInStringFormat_THEN_convertIntoPositionObject_2() {
 		String shipCoordinatesAndOrientation = "(12,2342,E)";
-		List<Position> pos = new Converter().convertShipPositionStringInputToPositionObject(shipCoordinatesAndOrientation);
+		List<Position> pos = converter.convertShipPositionStringInputToPositionObject(shipCoordinatesAndOrientation);
 		assertEquals(12, pos.get(0).getCurrentCoordinates().getX());
 		assertEquals(2342, pos.get(0).getCurrentCoordinates().getY());
 		assertEquals(Orientation.E,pos.get(0).getOrientation());
@@ -71,7 +79,7 @@ public class TestClient {
 	@Test
 	public void GIVEN_shipShotInStringFormat_THEN_convertIntoShotObject() {
 		String shotCoordinateInString = "(510,79)";
-		Coordinate shot = new Converter().convertShotInStringToShotObject(shotCoordinateInString);
+		Coordinate shot = converter.convertShotInStringToShotObject(shotCoordinateInString);
 		assertEquals(510, shot.getX());
 		assertEquals(79, shot.getY());
 	}
@@ -79,7 +87,7 @@ public class TestClient {
 	@Test
 	public void GIVEN_shipShotInStringFormat_THEN_convertIntoShotObject_2() {
 		String shotCoordinateInString = "(50,9)";
-		Coordinate shot = new Converter().convertShotInStringToShotObject(shotCoordinateInString);
+		Coordinate shot = converter.convertShotInStringToShotObject(shotCoordinateInString);
 		assertEquals(50, shot.getX());
 		assertEquals(9, shot.getY());
 	}
@@ -87,7 +95,7 @@ public class TestClient {
 	@Test
 	public void GIVEN_shipShotInStringFormat_THEN_convertIntoShotObject_3() {
 		String shotCoordinateInString = "(5,0)";
-		Coordinate shot = new Converter().convertShotInStringToShotObject(shotCoordinateInString);
+		Coordinate shot = converter.convertShotInStringToShotObject(shotCoordinateInString);
 		assertEquals(5, shot.getX());
 		assertEquals(0, shot.getY());
 	}
@@ -95,7 +103,7 @@ public class TestClient {
 	@Test
 	public void GIVEN_ShipMovementInStringFormat_THEN_ConvertItIntoShipsMovementObject() {
 		String shipMovementInString = "(1,2,LMMRRLM)";
-		List<Move> nextMoves = new Converter().convertShipMovementInStringToShipsListOfMove(shipMovementInString);
+		List<Move> nextMoves = converter.convertShipMovementInStringToShipsListOfMove(shipMovementInString);
 		assertEquals(Move.L, nextMoves.get(0));
 		assertEquals(Move.M, nextMoves.get(1));
 		assertEquals(Move.M, nextMoves.get(2));
@@ -108,7 +116,7 @@ public class TestClient {
 	@Test
 	public void GIVEN_ShipMovementInStringFormat_THEN_ConvertItIntoShipsMovementObject_2() {
 		String shipMovementInString = "(1,2,LMR)";
-		List<Move> nextMoves = new Converter().convertShipMovementInStringToShipsListOfMove(shipMovementInString);
+		List<Move> nextMoves = converter.convertShipMovementInStringToShipsListOfMove(shipMovementInString);
 		assertEquals(Move.L, nextMoves.get(0));
 		assertEquals(Move.M, nextMoves.get(1));
 		assertEquals(Move.R, nextMoves.get(2));
@@ -117,7 +125,7 @@ public class TestClient {
 	@Test
 	public void GIVEN_ShipMovementInStringFormat_THEN_ConvertItIntoShipsMovementObject_3() {
 		String shipMovementInString = "(1,2,MRLM)";
-		List<Move> nextMoves = new Converter().convertShipMovementInStringToShipsListOfMove(shipMovementInString);
+		List<Move> nextMoves = converter.convertShipMovementInStringToShipsListOfMove(shipMovementInString);
 		assertEquals(Move.M, nextMoves.get(0));
 		assertEquals(Move.R, nextMoves.get(1));
 		assertEquals(Move.L, nextMoves.get(2));
@@ -127,36 +135,49 @@ public class TestClient {
 	@Test(expected=InvalidInputException.class)
 	public void GIVEN_GridInputInIncorrectStringFormat_THEN_InvalidInputException() {
 		String gridCoordinateInString = "(5,7";
-		new Converter().convertGridStringInputToGridObject(gridCoordinateInString);
+		converter.convertGridStringInputToGridObject(gridCoordinateInString);
 	}
 	
 	@Test(expected=InvalidInputException.class)
 	public void GIVEN_GridInputInIncorrectStringFormat_THEN_InvalidInputException_2() {
 		String gridCoordinateInString = "5,7)";
-		new Converter().convertGridStringInputToGridObject(gridCoordinateInString);
+		converter.convertGridStringInputToGridObject(gridCoordinateInString);
 	}
 	
 	@Test(expected=InvalidInputException.class)
 	public void GIVEN_GridInputInIncorrectStringFormat_THEN_InvalidInputException_3() {
 		String gridCoordinateInString = "(5x7)";
-		new Converter().convertGridStringInputToGridObject(gridCoordinateInString);
+		converter.convertGridStringInputToGridObject(gridCoordinateInString);
 	}
 	
 	@Test(expected=InvalidInputException.class)
 	public void GIVEN_ShipPositionInputInIncorrectStringFormat_THEN_InvalidInputException() {
 		String shipCoordinatesAndOrientation = "1,2,N)";
-		new Converter().convertShipPositionStringInputToPositionObject(shipCoordinatesAndOrientation);
+		converter.convertShipPositionStringInputToPositionObject(shipCoordinatesAndOrientation);
 	}
 	
 	@Test(expected=InvalidInputException.class)
 	public void GIVEN_ShipPositionInputInIncorrectStringFormat_THEN_InvalidInputException_2() {
 		String shipCoordinatesAndOrientation = "(1,2,X)";
-		new Converter().convertShipPositionStringInputToPositionObject(shipCoordinatesAndOrientation);
+		converter.convertShipPositionStringInputToPositionObject(shipCoordinatesAndOrientation);
 	}
 	
 	@Test
 	public void GIVEN_MultipleShipPositionInputInStringFormat_THEN_NoInvalidInputException() {
 		String shipCoordinatesAndOrientation = "(1,2,N) (12,34,W) (234,567,E)";
-		new Converter().convertShipPositionStringInputToPositionObject(shipCoordinatesAndOrientation);
+		List<Position> pos = converter.convertShipPositionStringInputToPositionObject(shipCoordinatesAndOrientation);
+		assertEquals(new Position(1, 2, Orientation.N),pos.get(0));
+		assertEquals(new Position(12, 34, Orientation.W),pos.get(1));
+		assertEquals(new Position(234, 567, Orientation.E),pos.get(2));
+	}
+	
+	@Test
+	public void GIVEN_MultipleShipPositionInputInStringFormat_THEN_NoInvalidInputException_2() {
+		String shipCoordinatesAndOrientation = "(1,2,N) (12,34,W) (234,567,E) (12,34,S)";
+		List<Position> pos =   converter.convertShipPositionStringInputToPositionObject(shipCoordinatesAndOrientation);
+		assertEquals(new Position(1, 2, Orientation.N),pos.get(0));
+		assertEquals(new Position(12, 34, Orientation.W),pos.get(1));
+		assertEquals(new Position(234, 567, Orientation.E),pos.get(2));
+		assertEquals(new Position(12, 34, Orientation.S),pos.get(3));
 	}
 }
